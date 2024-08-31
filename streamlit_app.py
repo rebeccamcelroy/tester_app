4f12e281ff7b516e375aa82989ac2b085d776c39
Rebecca McElroy <37399401+rebeccamcelroy@users.noreply.github.com>
2024-08-31 13:01:41 +1000

    catch for not in top 100 ever

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,26 +81,24 @@ name = name.lower()
 # check if the name is in the list
 if name in grouped.groups:
     st.write('That name is in the top 100. Scroll down for statistics, graphs, and predictions...')
-else:
-    st.write('That name is not in the top 100, safe to use!')
 
-# get the data for the name
-name_data = grouped.get_group(name)
-
-# Alternatively, if you need to filter by both name and gender:
-#name_data = grouped[(grouped['Name'] == name) & (grouped['Gender'] == gender)]
-
-# run the gaussian process regression
-x_future, x_full, y_full_mean, y_full_sigma  = run_gaussian_process_regression(name_data)
-
-# recapitalise the name for output 
-display_name = name.capitalize()
-
-# use dark theme
-plt.style.use('dark_background')
-
-tab1, tab2, tab3 = st.tabs(["Statistics", "Graph", "Predictions"])
-with tab1:
+    # get the data for the name
+    name_data = grouped.get_group(name)
+    
+    # Alternatively, if you need to filter by both name and gender:
+    #name_data = grouped[(grouped['Name'] == name) & (grouped['Gender'] == gender)]
+    
+    # run the gaussian process regression
+    x_future, x_full, y_full_mean, y_full_sigma  = run_gaussian_process_regression(name_data)
+    
+    # recapitalise the name for output 
+    display_name = name.capitalize()
+    
+    # use dark theme
+    plt.style.use('dark_background')
+    
+    tab1, tab2, tab3 = st.tabs(["Statistics", "Graph", "Predictions"])
+    with tab1:
     st.header("Statistics")
     # get the stats for the selected name 
     # check whether the name made the list in 2023
@@ -111,101 +109,107 @@ with tab1:
         number_2023 = name_data.loc[name_data['Year'] == 2023]['Number'].values[0]
         
         st.write(f":red[{display_name}] was ranked :red[{rank_2023}] in 2023, :red[{number_2023}] babies were called this." )
-
+    
     else:
         # when was the last year the name was in the top 100
         year_last = name_data['Year'].max()
         # what was the ranking of the name in the last year
         rank_last = name_data.loc[name_data['Year'] == year_last]['Rank'].values[0]
-
+    
         st.write(f":red[{display_name}] was last in the top 100 in {year_last} when it was ranked {rank_last}.")
-
+    
     # total number of babies with that name
     total = name_data['Number'].sum()
-
+    
     # how many unique years are there in the dataset?
     unique_years = name_data['Year'].nunique()
-
+    
     # average number of babies with that name per year
     mean = total / unique_years
-
+    
     st.write(f"On average :red[{int(mean)}] babies were named :red[{display_name}] in NSW each year over the past {unique_years} years.")
-
+    
     # it was most popular in which year
     max_year = name_data.loc[name_data['Rank'].idxmin()]['Year']
     # what was the highest ranking of that name
     max_rank = name_data.loc[name_data['Rank'].idxmin()]['Rank']
-
+    
     st.write(f":red[{display_name}] was most popular in {max_year}, when it was ranked {max_rank}.")
-
+    
     # future stats
     # what does the model think 
-
+    
     if 2023 in name_data['Year'].values:
         st.write(f":red[Warning, prediction optimizer under construction:]")
             # future stats
         # how many babies are predicted to be named that in 2033
         future_number = y_full_mean[-10]
-
+    
         # difference between 2023 and 2033
         ratio = future_number/number_2023
-
+    
         # what does this ratio mean?
         # if ratio is approximately 1 then the name is likely to stay the same
         # if ratio is less than 1 then the name is likely to decrease in popularity
         # if ratio is greater than 1 then the name is likely to increase in popularity
-
+    
         if ratio > 0.9 and ratio < 1.1:
             st.write(f"The number of babies named {display_name} in the future is likely to stay about the same.")
-
+    
         elif ratio < 0.9:
             st.write(f"The number of babies named :red[{display_name}] in the future is likely to decrease.")
-
+    
         elif ratio > 1.1:
             st.write(f"The number of babies named :red[{display_name}] in the future is likely to increase.")
-
+    
         st.write(f":red[R = {ratio}]")
-
+    
     else:
         st.write(f":red[{display_name}] wasn't in the top 100 names last year, so it is safe to use. ")
-
-
+    
+    
              
-
-
-with tab2:
+    
+    
+    with tab2:
     st.header(display_name+" over time")
     # plot the name prevalence over time 
     plt.figure()
-
+    
     fig, ax = plt.subplots()
-
+    
     ax.plot(name_data['Year'], name_data['Number'], color='white')
-
+    
     ax.set_xlabel('Year')
     ax.set_ylabel('Number')
-
+    
     st.pyplot(fig)
-
-
-with tab3:
-
+    
+    
+    with tab3:
+    
     st.header("Predictions for "+display_name)
     # plot the name prevalence over time with the fit and prediction
     plt.figure()
-
+    
     fig, ax = plt.subplots()
-
+    
     ax.plot(name_data['Year'], name_data['Number'], label='Data', color='white')
     ax.plot(x_full, y_full_mean, 'lightcoral', label='Prediction')
     #ax.fill_between(x_full.ravel(), y_full_mean - 1.96 * y_full_sigma, 
     #            y_full_mean + 1.96 * y_full_sigma, alpha=0.2, color='blue')
-
+    
     ax.set_xlabel('Year')
     ax.set_ylabel('Number')
     ax.legend()
-
+    
     st.pyplot(fig)
+
+
+else:
+    st.write('That name has never been in the top 100, it must be unique!!')
+
+
 
 # with tab4:
 #     st.header("Predictions for "+display_name)
